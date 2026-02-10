52eaafa434abf293f7ca8c4c6ae64ca08ec65207
Marcos <marcosaurelioce@hotmail.com>
2026-02-10 14:33:55 -0300

    Adicionando novas features find's

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -3,16 +3,17 @@ package com.devsuperior.dscommerce.controllers;
 
 
 import com.devsuperior.dscommerce.dto.ProductDTO;
-import com.devsuperior.dscommerce.entities.Product;
 import com.devsuperior.dscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/products")
@@ -26,9 +27,9 @@ public class ProductController {
         return productService.findById(id);
     }
 
-    @GetMapping(value = "/{id}")
-    public List<ProductDTO> findAll() {
-        return productService.findAll();
+    @GetMapping
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        return productService.findAll(pageable);
     }
 
 }
