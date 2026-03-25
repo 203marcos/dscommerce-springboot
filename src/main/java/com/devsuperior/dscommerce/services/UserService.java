19de845eeaa32901b070331bb5494c5c01c77b96
Marcos <marcosaurelioce@hotmail.com>
2026-03-25 14:07:21 -0300

    feat(user): add roles and endpoint users/me

---
+++ src/main/java/com/devsuperior/dscommerce/services/UserService.java
@@ -0,0 +1,33 @@
+package com.devsuperior.dscommerce.services;
+
+import com.devsuperior.dscommerce.dto.UserDTO;
+import com.devsuperior.dscommerce.entities.User;
+import com.devsuperior.dscommerce.repositories.UserRepository;
+import com.devsuperior.dscommerce.services.exceptions.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Transactional(readOnly = true)
+    public UserDTO authenticatedUser() {
+        User user = authenticated();
+        return new UserDTO(user);
+    }
+
+    @Transactional(readOnly = true)
+    public User authenticated() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        return userRepository.findByEmail(username)
+                .orElseThrow(() -> new ResourceNotFoundException("Authenticated user not found"));
+    }
+}
+
